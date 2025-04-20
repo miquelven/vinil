51d5021d136a0d6ad9a6975989275ba462e99742
miquelven <miquelven.silva@gmail.com>
2025-04-19 23:46:00 -0300

    feat: add animations with @gsap/react

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,24 +1,79 @@
+"use client";
+
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import homeVinilImg from "@/assets/images/home-vinil.svg";
 import TextDecoration from "../text-decoration";
 import { ChevronsDown } from "lucide-react";
 
+gsap.registerPlugin(useGSAP);
+
 export default function Home() {
+  const container = useRef(null);
+  const vinilRef = useRef<HTMLImageElement>(null);
+
+  useGSAP(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline();
+
+      tl.fromTo(
+        ".vinil",
+        { opacity: 0 },
+        {
+          opacity: 1,
+          duration: 3.2,
+          ease: "power1.out",
+          delay: 0.3,
+        }
+      )
+        .fromTo(
+          ".title",
+          { y: 40, opacity: 0 },
+          { y: 0, opacity: 1, duration: 1, ease: "power2.out" },
+          "-=2.5"
+        )
+        .fromTo(
+          ".chevron",
+          { opacity: 0 },
+          { opacity: 1, duration: 1, ease: "power1.out" },
+          "-=1.7"
+        );
+
+      gsap.to(vinilRef.current, {
+        rotation: 360,
+        duration: 20,
+        ease: "linear",
+        repeat: -1,
+        transformOrigin: "50% 50%",
+      });
+    }, container);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <main className="flex justify-center items-center flex-col relative h-[75vh] pt-[47vh]">
+    <main
+      ref={container}
+      className="flex justify-center items-center flex-col relative h-[75vh] pt-[47vh]"
+    >
       <div className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/2">
         <img
+          ref={vinilRef}
           src={homeVinilImg}
           alt="Imagem do vinil da home"
-          className="animate-slow-spin"
+          className="vinil h-[80%] max-h-[90vh] w-auto"
         />
       </div>
-      <h1 className="font-primary font-bold text-[80px]">
+
+      <h1 className="title font-primary font-bold text-[80px]">
         Antes de ser{" "}
         <TextDecoration text={"ouvido,"} className="text-primary" /> o vinil é{" "}
         <TextDecoration text={"sentido."} className="text-primary" />
       </h1>
+
       <ChevronsDown
-        className=" animate-soft-bounce-down"
+        className="chevron animate-soft-bounce-down"
         color="#f5e9da"
         size={60}
       />
