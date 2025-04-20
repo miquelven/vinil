1cde928cf6c647c285b67df6e6a8f0284bc95f1e
miquelven <miquelven.silva@gmail.com>
2025-04-20 16:43:52 -0300

    feat: add visual tracker player

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "./components/home";
+import VisualTrackPlayer from "./components/visual-track-player";
 
 function App() {
   return (
     <div className="bg-dark">
       <Home />
+      <VisualTrackPlayer />
     </div>
   );
 }
